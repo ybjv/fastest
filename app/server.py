ca2995925aec2a3e47f11b3e2a0926875957a98b
ybjv <48372499+ybjv@users.noreply.github.com>
2019-11-27 15:23:38 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,9 +61,12 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
+    predp = None
+    if str(prediction) == 'crutches':
+        predp = 'Works!'
 
     
-    return JSONResponse({'result': str(prediction) })
+    return JSONResponse({'result': predp })
 
 
 if __name__ == '__main__':
