85b5093eefc3c67802ff74d360b422039c973c4b
ybjv <48372499+ybjv@users.noreply.github.com>
2019-11-27 15:14:24 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -62,10 +62,9 @@ async def analyze(request):
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     
-    if str(prediction) == 'elderly' or str(prediction) == 'crutches' or str(prediction) == 'stick' or str(prediction) == 'pregnant':
-        return JSONResponse({'result': 'Result = ' + prediction + '. Enjoy your seat!'})
-    else:
-        return JSONResponse({'result': 'Result = ' + prediction + '. Please give up your seat to the needy. Thank you!'})
+    prediction = 'Result = ' + prediction
+    
+    return JSONResponse({'result': str(prediction) })
 
 
 if __name__ == '__main__':
