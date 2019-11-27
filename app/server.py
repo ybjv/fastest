5581f25f73abfa202cbfc2f5bd4d61a5f9fdc361
ybjv <48372499+ybjv@users.noreply.github.com>
2019-11-27 16:58:15 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/x4yxrqd84e1rfoh/export%20%284%29.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/8421jrt3bz7xv0p/export%20%285%29.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['adults', 'crutches', 'elderly', 'kids', 'pregnant', 'stick']
